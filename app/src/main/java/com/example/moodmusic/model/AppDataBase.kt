3c0a073505c6a367742e442a08982f14078ee684
Andres Guzman <cesarandresguzman27@gmail.com>
2026-04-15 10:55:48 -0500

    Se organizo y actualizo la version de la db

--- app/src/main/java/com/example/moodmusic/model/AppDataBase.kt
+++ app/src/main/java/com/example/moodmusic/model/AppDataBase.kt
@@ -3,8 +3,8 @@ package com.example.moodmusic.model
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-// Se incrementó la versión de 1 a 2 debido al cambio en EstadoAnimoEntity (se añadió el campo avatar)
-@Database(entities = [UsuarioEntity::class, EstadoAnimoEntity::class], version = 2)
+// Se incrementó la versión a 3 para forzar la recreación de la base de datos tras cambios en las entidades
+@Database(entities = [UsuarioEntity::class, EstadoAnimoEntity::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun usuarioDao(): UsuarioDao
     abstract fun estadoAnimoDao(): EstadoAnimoDao
